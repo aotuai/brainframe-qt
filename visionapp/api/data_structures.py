d2af9040c067bfd8935ef5cab8877c0f33efcbd4
alex <alex.thiel@dilililabs.com>
2018-03-09 21:57:10 -0700

    implemented data structure serialization and deserialization, and implemented all tests related to this.

--- visionapp/api/data_structures.py
+++ visionapp/api/data_structures.py
@@ -2,54 +2,12 @@ import abc
 
 
 class Dictable(abc.ABC):
-
     @abc.abstractstaticmethod
     def from_dict(d):
         pass
 
     @abc.abstractmethod
-    def to_dict(self, d):
-        pass
-
-
-class ZoneStatus(Dictable):
-    """
-    {"zone": Zone,
-     "tstamp": ms,
-     "total_count": {"person": 100, "dog": 5},
-     "objects": {"person": [Detection, Detection], "dog": [Detection]}
-     "alerts": [Alert, Alert]
-     }
-    """
-    def to_dict(self, d):
-        pass
-
-    def __init__(self, zone, tstamp, total_count, objects, alerts):
-        pass
-
-    @staticmethod
-    def from_dict(d):
-        zone = Zone.from_dict(j["zone"])
-        tstamp = j['tstamp']
-        total_count = j['total_count']
-        objects = [Detection.from_dict(det) for det in j["objects"]]
-        alerts = [Alert.from_dict(alert) for alert in j["alerts"]]
-        return ZoneStatus(zone, tstamp, total_count, objects, alerts)
-
-
-class Zone(Dictable):
-    """
-    {"name": str,
-    "id": int,
-    "alarms": [Alarm, Alarm, Alarm]
-    "coords": [[x1, y1], [x2, y2], [x3, y3], [x4, y4] ... [xn, yn]]
-    """
-
-    def to_dict(self, d):
-        pass
-
-    @staticmethod
-    def from_dict(d):
+    def to_dict(self):
         pass
 
 
@@ -59,12 +17,20 @@ class Detection(Dictable):
     "rect": [x1, y1, x2, y2],
     "attributes": ["boy", "drinking_water"]}
     """
-    def to_dict(self, d):
-        pass
+
+    def __init__(self, class_name, rect, attributes):
+        self.class_name = class_name
+        self.rect = rect
+        self.attributes = attributes
+
+    def to_dict(self):
+        return self.__dict__
 
     @staticmethod
     def from_dict(d):
-        pass
+        return Detection(class_name=d["class_name"],
+                         rect=d["rect"],
+                         attributes=d["attributes"])
 
 
 class Alarm(Dictable):
@@ -76,15 +42,39 @@ class Alarm(Dictable):
     "check_value": The int value or threshhold to be over, under, or equal to,
     "test": >, <, !=, == (as string),
     "use_active_time": bool,
-    "active_time_start":  "HH:MM" using 24 hour time,
-    "active_time_end": "HH:MM" using 24 hour time}
+    "start_time":  "HH:MM" using 24 hour time,
+    "end_time": "HH:MM" using 24 hour time}
     """
-    def to_dict(self, d):
-        pass
+
+    def __init__(self, name, id_, test, check_value,
+                 with_class_name, with_attributes,
+                 use_active_time, active_time_start, active_time_end):
+        self.name = name
+        self.id_ = id_
+        self.test = test
+        self.check_value = check_value
+        self.with_class_name = with_class_name
+        self.with_attributes = with_attributes
+        self.use_active_time = use_active_time
+        self.active_time_start = active_time_start
+        self.active_time_end = active_time_end
+
+    def to_dict(self):
+        d = self.__dict__
+        d["id"] = d.pop("id_")
+        return d
 
     @staticmethod
     def from_dict(d):
-        pass
+        return Alarm(name=d["name"],
+                     id_=d["id"],
+                     test=d["test"],
+                     check_value=d["check_value"],
+                     with_class_name=d["with_class_name"],
+                     with_attributes=d["with_attributes"],
+                     use_active_time=d["use_active_time"],
+                     active_time_start=d["active_time_start"],
+                     active_time_end=d["active_time_end"])
 
 
 class Alert(Dictable):
@@ -98,55 +88,163 @@ class Alert(Dictable):
     }
     """
 
+    def __init__(self, id_, alarm_id, start_time, end_time):
+        self.id_ = id_
+        self.alarm_id = alarm_id
+        self.start_time = start_time
+        self.end_time = end_time
 
-# Stream Data structures
-class StreamConfiguration(Dictable):
-    """
-    A stream configuration can only be gotten from the API, and has an
-    ID.
-    {"name": str, "id": int, "config": Some camera config object}
-    """
-
-    def to_dict(self, d):
-        pass
+    def to_dict(self):
+        d = self.__dict__
+        d["id"] = d.pop("id_")
+        return d
 
     @staticmethod
     def from_dict(d):
-        pass
+        return Alert(id_=d["id"],
+                     alarm_id=d["alarm_id"],
+                     start_time=d["start_time"],
+                     end_time=d["end_time"])
 
 
+class Zone(Dictable):
+    """
+    {"name": str,
+    "id": int,
+    "alarms": [Alarm, Alarm, Alarm]
+    "coords": [[x1, y1], [x2, y2], [x3, y3], [x4, y4] ... [xn, yn]]
+    """
+
+    def __init__(self, name, id_, alarms, coords):
+        self.name = name
+        self.id_ = id_
+        self.alarms = alarms
+        self.coords = coords
+
+    def to_dict(self):
+        d = self.__dict__
+        alarms = [alarm.to_dict() for alarm in self.alarms]
+        d["id"] = d.pop("id_")
+        d["alarms"] = alarms
+        return d
+
+    @staticmethod
+    def from_dict(d):
+        alarms = [Alarm.from_dict(alarm_d) for alarm_d in d["alarms"]]
+        return Zone(name=d["name"],
+                    id_=d["id"],
+                    alarms=alarms,
+                    coords=d["coords"])
+
+
+class ZoneStatus(Dictable):
+    """
+    {"zone": Zone,
+     "tstamp": ms,
+     "total_counts": {"person": 100, "dog": 5},
+     "detections": {"person": [Detection, Detection], "dog": [Detection]}
+     "alerts": [Alert, Alert]
+     }
+    """
+
+    def __init__(self, zone, tstamp, total_counts, detections, alerts):
+        self.zone = zone
+        self.tstamp = tstamp
+        self.total_counts = total_counts
+        self.detections = detections
+        self.alerts = alerts
+
+    def to_dict(self):
+        d = self.__dict__
+        d["zone"] = self.zone.to_dict()
+        d["detections"] = [det.to_dict() for det in self.detections]
+        d["alerts"] = [alert.to_dict() for alert in self.alerts]
+        return d
+
+    @staticmethod
+    def from_dict(d):
+        zone = Zone.from_dict(d["zone"])
+        detections = [Detection.from_dict(det) for det in d["detections"]]
+        alerts = [Alert.from_dict(alert) for alert in d["alerts"]]
+        return ZoneStatus(zone=zone,
+                          tstamp=d["tstamp"],
+                          total_counts=d["total_counts"],
+                          detections=detections,
+                          alerts=alerts)
+
+
+# Stream Data structures
 class WebcamConfig(Dictable):
     """
     {"device_index": int}
     """
     CONN_TYPE = "Webcam"
 
-    def to_dict(self, d):
-        pass
+    def __init__(self, device_index):
+        self.device_index = device_index
+
+    def to_dict(self):
+        return self.__dict__
 
     @staticmethod
     def from_dict(d):
-        pass
-
+        return WebcamConfig(device_index=d["device_index"])
 
 class IPPasswordConfig(Dictable):
     """
     {"url": valid URL, "username": str, "password": str}
     """
-    TYPE = "IPPassword"
+    CONN_TYPE = "IPPassword"
 
-    def to_dict(self, d):
-        pass
+    def __init__(self, url, username, password):
+        self.url = url
+        self.username = username
+        self.password = password
+
+    def to_dict(self):
+        return self.__dict__
 
     @staticmethod
     def from_dict(d):
-        pass
+        return IPPasswordConfig(url=d["url"],
+                                username=d["username"],
+                                password=d["password"])
+
+class StreamConfiguration(Dictable):
+    """
+    A stream configuration can only be gotten from the API, and has an
+    ID.
+    {"name": str,
+    "id": int,
+    "connection_type": "Webcam" or "IPPassword"
+    "config": A serialized configuration
+    }
+    """
+    CONN_TYPES = {WebcamConfig.CONN_TYPE: WebcamConfig,
+                  IPPasswordConfig.CONN_TYPE: IPPasswordConfig}
+
+    def __init__(self, name, id_, connection_type, config):
+        self.name = name
+        self.id_ = id_
+        self.connection_type = connection_type
+        self.config = config
 
 
+    def to_dict(self):
+        d = self.__dict__
+        d["id"] = d.pop("id_")
+        d["config"] = self.config.to_dict()
+        return d
 
-
-
-
+    @staticmethod
+    def from_dict(d):
+        # Get the config
+        config_type = StreamConfiguration.CONN_TYPES[d["connection_type"]]
+        config = config_type.from_dict(d["config"])
+        return StreamConfiguration(name=d["name"],
+                                   id_=d["id"],
+                                   connection_type=d["connection_type"],
+                                   config=config)
 
 
 
