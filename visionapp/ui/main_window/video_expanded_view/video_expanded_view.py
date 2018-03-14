263038041fcab43ecdc9029e6a7b07271028fd56
Bryce Beagle <bryce.beagle@dilililabs.com>
2018-03-13 19:44:02 -0700

    Started connecting up slots and signals

--- visionapp/ui/main_window/video_expanded_view/video_expanded_view.py
+++ visionapp/ui/main_window/video_expanded_view/video_expanded_view.py
@@ -1,11 +1,52 @@
-from PyQt5.QtWidgets import QWidget
+from PyQt5.QtCore import pyqtSignal, pyqtSlot
+from PyQt5.QtWidgets import QHBoxLayout, QWidget
 from PyQt5.uic import loadUi
+
+from .video_large.video_large import VideoLarge
 
 
 class VideoExpandedView(QWidget):
+    """Holds the expanded video view. Hidden when no stream selected"""
+
+    expanded_stream_closed_signal = pyqtSignal()
+    """Signaled when expanded stream is closed"""
 
     def __init__(self, parent):
-
         super().__init__(parent)
 
         loadUi("ui/main_window/video_expanded_view/video_expanded_view.ui")
+
+        # TODO: Migrate to QtDesigner
+        self.layout_ = QHBoxLayout(self)
+        self.setLayout(self.layout_)
+        self.layout_.addWidget(VideoLarge(self, 5))
+
+        self.sizePolicy().setHorizontalStretch(0)
+
+    @pyqtSlot(int)
+    def open_expanded_view_slot(self, stream_id):
+        """Signaled by thumbnail view when thumbnail video is clicked"""
+
+        # self.expanded_video.change_stream(stream_id)
+        # self.expanded_video.setHidden(False)
+        # self.alert_log.setHidden(False)
+
+        self.sizePolicy().setHorizontalStretch(5)
+        print("Event")
+
+        # Create new VideoLarge()
+        # Create new AlertLog()
+        # Adjust layout width
+
+    @pyqtSlot()
+    def expanded_stream_closed_slot(self):
+        """Signaled by expanded video widget when widget is closed"""
+
+        # self.expanded_video.change_stream(None)
+
+        # self.expanded_video.setHidden(True)
+        # self.alert_log.setHidden(False)
+
+        # Alert slots that expanded stream was closed
+        # VideoThumbnailView will remove highlight from thumbnail video
+        self.expanded_stream_closed_signal.emit()
--- visionapp/ui/main_window/video_thumbnail_view/video_small/video_small.py
+++ visionapp/ui/main_window/video_thumbnail_view/video_small/video_small.py
@@ -1,12 +1,45 @@
-from PyQt5.QtWidgets import QGraphicsScene, QGraphicsView
+from PyQt5.QtCore import pyqtSignal, pyqtSlot
+
+from ui.resources.stream_widget import StreamWidget
 
 
-class VideoSmall(QGraphicsView):
+class VideoSmall(StreamWidget):
+    """Video for ThumbnailView"""
 
-    def __init__(self, parent):
+    stream_clicked = pyqtSignal(int)
 
-        super().__init__(parent)
+    def __init__(self, parent=None, frame_rate=30):
+        super().__init__(frame_rate, parent)
 
-        self.scene_ = QGraphicsScene()
-        self.setScene(self.scene_)
+        self.parent = parent
 
+        # Because these widgets are added dynamically, we can't connect slots
+        # and signals using QtDesigner and have to do it manually
+        if parent is not None:  # Required or QtDesigner throws errors
+            self.stream_clicked.connect(parent.thumbnail_stream_clicked_slot)
+            parent.remove_selection_border_signal.connect(
+                self.remove_selection_border_slot)
+
+        self.stream_id = 1234  # TODO
+
+    def mouseReleaseEvent(self, event):
+
+        print("Moused button released")
+
+        # Add border around stream to indicate its selection
+        self.add_selection_border()
+
+        self.stream_clicked.emit(self.stream_id)
+
+        super().mousePressEvent(event)
+
+    def add_selection_border(self):
+        """Add border around stream"""
+        pass
+
+    @pyqtSlot(int)
+    def remove_selection_border_slot(self, stream_id):
+        """Remove border around stream if stream_id matches self.id_"""
+        if self.stream_id == stream_id:
+            # TODO: Remove border
+            pass
