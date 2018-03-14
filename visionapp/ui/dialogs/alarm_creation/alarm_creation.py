9a02131a3e1f065e567ba3c14d536a362d454da9
Bryce Beagle <bryce.beagle@dilililabs.com>
2018-03-13 19:42:01 -0700

    Alarm creation dialog

--- visionapp/ui/dialogs/alarm_creation/alarm_creation.py
+++ visionapp/ui/dialogs/alarm_creation/alarm_creation.py
@@ -1,9 +1,22 @@
-from PyQt5.QtWidgets import QWidget
+from PyQt5.QtWidgets import QDialog
+from PyQt5.uic import loadUi
 
 
-class AlarmCreationDialog(QWidget):
+class AlarmCreationDialog(QDialog):
 
     def __init__(self, parent):
 
         super().__init__(parent)
 
+        loadUi("ui/dialogs/alarm_creation/alarm_creation.ui", self)
+
+
+if __name__ == '__main__':
+
+    from PyQt5.QtWidgets import QApplication
+
+    app = QApplication([])
+    window = AlarmCreationDialog(None)
+    window.show()
+
+    app.exec_()
