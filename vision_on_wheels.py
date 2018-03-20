dc55ca502059014e8899aefcefa912c2c0985f07
Bryce Beagle <bryce.beagle@dilililabs.com>
2018-03-20 16:37:20 -0700

    Improved import for visionapp external use

--- vision_on_wheels.py
+++ vision_on_wheels.py
@@ -3,12 +3,11 @@ import sys
 
 from PyQt5.QtWidgets import QApplication
 
-# TODO: Wrap this up in something nicer
-import api
-from visionapp.client.ui.main_window.main_window import MainWindow
+from visionapp import api, MainWindow
 
 if __name__ == '__main__':
 
+    # Monkeypatch the api to be an instantiated object
     api.api = api.API("localhost", 80)
 
     # Ensure that all relative paths are correct
