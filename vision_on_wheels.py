59ed776388d169cb6b4a131b95784064258fa9b9
Bryce Beagle <bryce.beagle@dilililabs.com>
2018-03-20 11:07:49 -0700

    Path adjusted to directory changes

--- vision_on_wheels.py
+++ vision_on_wheels.py
@@ -9,7 +9,7 @@ from visionapp.client.ui.main import Main
 if __name__ == '__main__':
 
     # Ensure that all relative paths are correct
-    os.chdir(os.path.dirname(__file__) + "/visionapp")
+    os.chdir(os.path.dirname(__file__) + "/visionapp/client")
 
     app = QApplication(sys.argv)
     window = Main()
