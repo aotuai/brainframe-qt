0e840d6c34db5dded483180137eb8857cdd1fa0c
Bryce Beagle <bryce.beagle@dilililabs.com>
2018-03-24 18:45:12 -0700

    Clicking on expanded video now draws a circle.
    This is a proof of concept to get mousevents correct. Will be used for
    drawing zones in the future

--- visionapp/client/ui/main_window/video_expanded_view/video_large/video_large.py
+++ visionapp/client/ui/main_window/video_expanded_view/video_large/video_large.py
@@ -1,3 +1,7 @@
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QMouseEvent, QPen
+from PyQt5.QtWidgets import QGraphicsEllipseItem
+
 from visionapp.client.ui.resources.stream_widget import StreamWidget
 
 
@@ -6,3 +10,19 @@ class VideoLarge(StreamWidget):
     def __init__(self, parent=None, stream_conf=None, frame_rate=30):
 
         super().__init__(stream_conf, frame_rate, parent)
+
+    def mouseReleaseEvent(self, event: QMouseEvent):
+
+        click = self.mapToScene(event.pos())
+
+        print(event.pos(), click)
+
+        circle = QGraphicsEllipseItem(click.x() - 5, click.y() - 5,
+                                      10, 10)
+        pen = circle.pen()
+        pen.setColor(Qt.red)
+        circle.setPen(pen)
+
+        self.scene_.addItem(circle)
+
+        super().mouseReleaseEvent(event)
