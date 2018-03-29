12d3cc19bbf5a734794f6cb3d8cc4157cd8e84c1
Bryce Beagle <bryce.beagle@dilililabs.com>
2018-03-29 13:49:51 -0700

    Removed if "__name__" == __main__ from main script

--- vision_on_wheels.py
+++ vision_on_wheels.py
@@ -5,16 +5,16 @@ from PyQt5.QtWidgets import QApplication
 
 from visionapp.client import api, MainWindow
 
-if __name__ == '__main__':
-    # Monkeypatch the api to be an instantiated object
-    api.__dict__['api'] = api.API("http://localhost", 8000)
 
-    # Ensure that all relative paths are correct
-    os.chdir(os.path.dirname(__file__))
+# Monkeypatch the api to be an instantiated object
+api.__dict__['api'] = api.API("http://localhost", 8000)
 
-    app = QApplication(sys.argv)
-    window = MainWindow()
+# Ensure that all relative paths are correct
+os.chdir(os.path.dirname(__file__))
 
-    app.exec_()
+app = QApplication(sys.argv)
+window = MainWindow()
 
-    api.api.close()
+app.exec_()
+
+api.api.close()
