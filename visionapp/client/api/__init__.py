f8b4e4f303a0408b15063b7daece1287d91210bc
Bryce Beagle <bryce.beagle@dilililabs.com>
2018-03-29 20:54:18 -0700

    api/__init__.py now has mock object so that QtDesigner can init without API

--- visionapp/client/api/__init__.py
+++ visionapp/client/api/__init__.py
@@ -1,4 +1,18 @@
 from visionapp.client.api.api_stub import API, APIError
 
+
+class _MockAPI:
+    """Mock API object so that QtDesigner can initialize without API instance"""
+
+    def __getattr__(self, item):
+        """If __getattribute__ fails, this is called"""
+        return None
+
+    @staticmethod
+    def get_stream_configurations():
+        return []
+
+
 # API instance that is later monkeypatched to be a singleton
-api = None  # type: API  # TODO: Use Py3.6 variable annotations when available
+# TODO: Use Py3.6 variable annotations when available
+api = _MockAPI()  # type: API
