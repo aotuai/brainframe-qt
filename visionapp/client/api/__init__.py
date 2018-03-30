38891b580490f989f395f63e9f05a4c2f9f9a651
Bryce Beagle <bryce.beagle@dilililabs.com>
2018-03-29 18:36:59 -0700

    Added type hinting to api/__init__.py

--- visionapp/client/api/__init__.py
+++ visionapp/client/api/__init__.py
@@ -1,4 +1,4 @@
 from visionapp.client.api.api_stub import API, APIError
 
 # API instance that is later monkeypatched to be a singleton
-api = None
+api = None  # type: API  # TODO: Use Py3.6 variable annotations when available
