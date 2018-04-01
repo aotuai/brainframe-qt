58f0af11747169358f62284b8b604d16c4f75332
alex <alex.thiel@dilililabs.com>
2018-03-31 20:21:12 -0700

    Fixed relative pathing mistake

--- visionapp/client/ui/resources/video_items/__init__.py
+++ visionapp/client/ui/resources/video_items/__init__.py
@@ -1,5 +1,6 @@
 from .click_circle import ClickCircle
 from .stream_polygon import StreamPolygon
-from ui.resources.video_items.stream_detections import DetectionPolygon, \
+from visionapp.client.ui.resources.video_items.stream_detections import \
+    DetectionPolygon, \
     ZoneStatusPolygon
 from .stream_widget import StreamWidget
