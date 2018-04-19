fdbbd8546944b57e36a8618f2e25c121bbdda209
Bryce Beagle <bryce.beagle@dilililabs.com>
2018-04-19 14:15:10 -0700

    UI now waits for server initialization before opening

--- vision_on_wheels.py
+++ vision_on_wheels.py
@@ -1,6 +1,7 @@
-import os
-import sys
 from argparse import ArgumentParser
+from requests.exceptions import ConnectionError
+import sys
+import time
 
 from PyQt5.QtWidgets import QApplication
 from PyQt5.QtGui import QIcon
@@ -10,20 +11,31 @@ from visionapp.client.ui.resources.paths import image_paths
 
 # CLI Arguments
 parser = ArgumentParser(description="This runs the client VisionApp")
-parser.add_argument("-a", "--api-url", type=str, default="http://localhost:8000",
+parser.add_argument("-a", "--api-url", type=str,
+                    default="http://localhost:8000",
                     help="The URL that the server is currently running on. "
                          "This can be localhost, or a local IP, or a remote"
                          " IP depending on your setup.")
 args = parser.parse_args()
 
-
 # Monkeypatch the api to be an instantiated object
 api.__dict__['api'] = api.API(args.api_url)
 
-# Set all stream analysis as "active" here, since there's no way to in the UI
-configs = api.api.get_stream_configurations()
-for config in configs:
-    success = api.api.start_analyzing(config.id)
+print("Connecting to server...")
+while True:
+
+    try:
+        # Set all stream analysis as "active" here, since there is currently no
+        # way to in the UI
+        configs = api.api.get_stream_configurations()
+    except ConnectionError:
+        # Server not started yet
+        print("Server not running. Trying again in 2 seconds...")
+        time.sleep(2)
+        continue
+    for config in configs:
+        success = api.api.start_analyzing(config.id)
+    break
 
 # Run the UI
 app = QApplication(sys.argv)
