4393195d0bc09351ec714c90360009db1c003fdf
Tyler Compton <tyler.compton@dilililabs.com>
2018-05-22 19:02:50 -0700

    Add licenses to resources for pyinstaller

--- brainframe_qt/ui/resources/paths/text_paths.py
+++ brainframe_qt/ui/resources/paths/text_paths.py
@@ -3,3 +3,6 @@ from .all_resource_paths import route_path
 license_agreement_txt = route_path("license.txt")
 
 license_dir = route_path("brainframe", "shared", "licenses")
+
+for license_file in license_dir.iterdir():
+    route_path(license_file)
