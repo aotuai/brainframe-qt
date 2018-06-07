20d04898a663460423850a3fdaf64b5762ff1b96
alex <alex.thiel@dilililabs.com>
2018-06-06 18:13:04 -0700

    Fixed the SqliteQueue database issue in tests (everything passes now) substantially speeding them up. Also reorganized some things in the UI.

--- brainframe_qt/api/streaming.py
+++ brainframe_qt/api/streaming.py
@@ -5,96 +5,10 @@ from time import sleep, time
 
 import cv2
 
-from brainframe.client.api import codecs
 from brainframe.client.api.codecs import StreamConfiguration
+from brainframe.client.api.status_poller import StatusPoller
 
 
-class StatusPoller(Thread):
-    """ This solves the problem that multiple UI elements will want to know the
-    latest ZoneStatuses for any given stream. """
-
-    def __init__(self, api, ms_status_updates):
-        """
-        :param api: An API() object for interacting with the BrainFrame REST
-        api.
-        :param ms_status_updates: Miliseconds between calling for a zone status
-        update
-        """
-        super().__init__(name="StatusPollerThread")
-        self._api = api
-        self._seconds_between_updates = ms_status_updates / 1000
-        self._running = False
-
-        # Get something before starting the thread
-        self._latest = self._api.get_latest_zone_statuses()
-        self.start()
-
-    def run(self):
-        """Polls Brainserver for ZoneStatuses at a constant rate"""
-        self._running = True
-        call_time = 0
-        while self._running:
-
-            # Call the server, timing how long the call takes
-            try:
-                start = time()
-                latest = self._api.get_latest_zone_statuses()
-                self._latest = latest
-                call_time = time() - start
-            except ConnectionError:
-                logging.warning("StatusLogger: Could not reach server!")
-                sleep(2)
-
-            # Sleep for the appropriate amount to keep call times consistent
-            time_left = self._seconds_between_updates - call_time
-            if time_left > 0:
-                sleep(time_left)
-
-        self._running = False
-
-    @property
-    def is_running(self):
-        return self._running
-
-    def get_alarm(self, stream_id, alarm_id) -> Union[None, codecs.ZoneAlarm]:
-        """Return a list of unverified alerts for this stream_id"""
-        zones = self.get_zones(stream_id)
-        if len(zones) == 0:
-            return None
-        alarms = sum([zone.alarms for zone in zones], [])
-        alarms = [alarm for alarm in alarms if alarm.id == alarm_id]
-        if len(alarms) == 0:
-            return None
-        return alarms[0]
-
-    def get_zones(self, stream_id) -> List[codecs.Zone]:
-        statuses = self.get_latest_statuses(stream_id)
-        zones = [status.zone for status in statuses]
-        return zones
-
-    def get_detections(self, stream_id) -> Tuple[float, List[codecs.Detection]]:
-        """Conveniently return all detections found in this stream and
-        their respective timestamp in which they were detected."""
-        statuses = self.get_latest_statuses(stream_id)
-        if len(statuses) == 0:
-            return 0, []
-
-        # Find the main screen
-        status = [status for status in statuses
-                  if status.zone.name == "Screen"][0]
-        return status.tstamp, status.detections
-
-    def get_latest_statuses(self, stream_id) -> List[codecs.ZoneStatus]:
-        """Returns the latest cached list of ZoneStatuses for that stream_id"""
-        latest = self._latest
-        if stream_id not in latest:
-            return []
-        return latest[stream_id]
-
-    def close(self):
-        """Close the status polling thread"""
-        self._running = False
-        self.join()
 
 
 class ProcessedFrame:
@@ -113,7 +27,7 @@ class SyncedStreamReader(Thread):
 
     def __init__(self,
                  url: str,
-                 stream_config: StreamConfiguration,
+                 stream_id: int,
                  status_poller: StatusPoller):
         """Creates a new SyncedStreamReader.
 
@@ -123,7 +37,7 @@ class SyncedStreamReader(Thread):
         """
         super().__init__(name="SyncedStreamReaderThread")
         self.url = url
-        self.stream_config = stream_config
+        self.stream_id = stream_id
         self.status_poller = status_poller
 
         self._running = False
@@ -196,7 +110,7 @@ class SyncedStreamReader(Thread):
 
             # Get the latest zone statuses
             zone_statuses = self.status_poller.get_latest_statuses(
-                self.stream_config.id)
+                self.stream_id)
             tstamp = zone_statuses[-1].tstamp if zone_statuses else None
 
             if zone_statuses and last_inference_tstamp != tstamp:
@@ -214,18 +128,13 @@ class SyncedStreamReader(Thread):
                     frame.frame,
                     frame.tstamp,
                     zone_statuses)
-
+            print("Debug: ", self.stream_id, len(frame_buf))
             # Drain the buffer if it is getting too large
             while len(frame_buf) > self.MAX_BUF_SIZE:
                 frame_buf.pop(0)
 
         logging.info("SyncedStreamReader: Closing")
         self._running = False
-
-    def _update_latest(self, zone_statuses):
-        """Check if the zone statuses correspond to any frames in the buffer and
-        update the latest if so.
-        """
 
     def close(self):
         self._running = False
@@ -255,7 +164,7 @@ class StreamManager:
 
         if url not in self._stream_readers:
             stream_reader = SyncedStreamReader(
-                url, stream_config, self._status_poller)
+                url, stream_config.id, self._status_poller)
             self._stream_readers[url] = stream_reader
             return stream_reader
 
@@ -263,7 +172,7 @@ class StreamManager:
             # Stream not running. Starting a new one.
             self._stream_readers[url].close()
             stream_reader = SyncedStreamReader(
-                url, stream_config, self._status_poller)
+                url, stream_config.id, self._status_poller)
             return stream_reader
 
         return self._stream_readers[url]
