14bc2c7fa648cb5a03f932a7dfd6fccadfb8c4b7
Bryce Beagle <bryce.beagle@dilililabs.com>
2018-08-03 17:35:22 -0700

    Resolves BF-324: Grey bar at bottom of thumbnail stream widget should display stream name

--- brainframe_qt/ui/main_window/video_thumbnail_view/thumbnail_grid_layout/video_small/video_small.py
+++ brainframe_qt/ui/main_window/video_thumbnail_view/thumbnail_grid_layout/video_small/video_small.py
@@ -1,5 +1,5 @@
 from PyQt5.QtCore import pyqtSignal, QRectF, Qt
-from PyQt5.QtGui import QPainter, QColor, QImage
+from PyQt5.QtGui import QPainter, QColor, QImage, QFontMetricsF
 
 from brainframe.client.ui.resources.paths import image_paths
 from brainframe.client.ui.resources.video_items.stream_widget import (
@@ -85,42 +85,61 @@ class VideoSmall(StreamWidget):
         Overrides the super().drawForeground method which by default does pretty
         much nothing
         """
+        # Gray rectangle
+        brush = painter.brush()
+        brush.setColor(QColor(0, 0, 0, 127))  # Half transparent black
+        brush.setStyle(Qt.SolidPattern)
+        painter.setBrush(brush)
+
+        # No border
+        painter.setPen(Qt.NoPen)
+
+        width = self.scene().width()
+        height = self.scene().height()
+        bar_percent = .1
+
+        bar_height = width * bar_percent
+
+        # Draw alert background
+        painter.fillRect(0, int(height - bar_height),
+                         int(width), int(bar_height),
+                         painter.brush())
+
+        image_width_with_margins = 0
         if self.ongoing_alerts:
-            # Gray rectangle
-            brush = painter.brush()
-            brush.setColor(QColor(0, 0, 0, 127))  # Half transparent black
-            brush.setStyle(Qt.SolidPattern)
-            painter.setBrush(brush)
-
-            # No border
-            painter.setPen(Qt.NoPen)
-
-            # Draw alert background
-            painter.fillRect(0,
-                             int(self.sceneRect().height() * 2 / 3),
-                             int(self.sceneRect().width()),
-                             int(self.sceneRect().height() / 3),
-                             painter.brush())
-
-            # Draw text
-            font = painter.font()
-            point_size = int(self.sceneRect().height() / 6)
-            font.setPointSize(point_size if point_size > 0 else 1)
-            painter.setFont(font)
-            painter.setPen(Qt.white)
-
-            painter.drawText(int(self.sceneRect().width() / 24),
-                             int(self.sceneRect().height() * 11 / 12), "Alert!")
-
             # Draw icon
+            image_percent = 0.8
+
             image = QImage(str(image_paths.alert_icon))
             image = image.scaled(int(self.sceneRect().width()),
-                                 int(self.sceneRect().height() / 6),
+                                 int(bar_height * image_percent),
                                  Qt.KeepAspectRatio)
+
+            image_margin = bar_height / 4
             painter.drawImage(
-                int(self.sceneRect().width() * 23 / 24) - image.width(),
-                int(self.sceneRect().height() * 11 / 12) - image.height(),
+                int(width - image.width() - image_margin),
+                int(height - image.height()
+                    - (bar_height * (1 - image_percent) / 2)),
                 image)
+
+            image_width_with_margins = image.width() + (2.5*image_margin)
+
+        # Draw text
+        font = painter.font()
+        point_size = bar_height / 2
+        font.setPointSizeF(point_size if point_size > 0 else 1)
+        painter.setFont(font)
+        painter.setPen(Qt.white)
+
+        font_metric = QFontMetricsF(font)
+        stream_name_text = font_metric.elidedText(
+            self.stream_conf.name,
+            Qt.ElideRight,
+            width - image_width_with_margins)
+
+        painter.drawText(int(point_size / 2),
+                         int(height - (point_size / 2)),
+                         stream_name_text)
 
     def mouseReleaseEvent(self, event):
         # Add border around stream to indicate its selection
