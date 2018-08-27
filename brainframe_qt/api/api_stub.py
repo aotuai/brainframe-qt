47297956db02b0502eacc9ac5ef5503629a6b62c
Tyler Compton <tyler.compton@dilililabs.com>
2018-08-27 13:42:16 -0700

    Add ability to add encodings by vector to API

--- brainframe_qt/api/api_stub.py
+++ brainframe_qt/api/api_stub.py
@@ -309,7 +309,8 @@ class API:
         """Saves and encodes an image under the identity with the given ID.
 
         :param identity_id: Identity to associate the image with
-        :param class_name: The class name to encode this image for
+        :param class_name: The type of object this image shows and should be
+            encoded for
         :param image: The image to save
         :return: The image ID
         """
@@ -335,6 +336,21 @@ class API:
 
         image_id = self._put_raw(req, image, mime_type)
         return image_id
+
+    def new_identity_vector(self, identity_id: int, class_name: str,
+                            vector: List[float]) -> int:
+        """Saves the given vector under the identity with the given ID. In this
+        case, a vector is simply a list of one or more numbers that describe
+        some object in an image.
+
+        :param identity_id: Identity to associate the vector with
+        :param class_name: The type of object this vector describes
+        :param vector: The vector to save
+        :return: The vector ID
+        """
+        req = f"/api/identities/{identity_id}/classes/{class_name}/vectors"
+
+        return self._put_json(req, ujson.dumps(vector))
 
     def get_identity_image(self, identity_id: int, class_name: str,
                            image_id: int) -> np.ndarray:
