4185f4b4799549c1e9cb4cb51a00b1281b176b00
Tyler Compton <tyler.compton@dilililabs.com>
2018-09-17 14:33:00 -0700

    Switch to using a decorator for kind_to_error_type

--- brainframe_qt/api/api_errors.py
+++ brainframe_qt/api/api_errors.py
@@ -7,6 +7,16 @@ kind_to_error_type = {}
 """Maps error kinds to their corresponding error type."""
 
 
+def register_error_type(cls):
+    """A class decorator. Registers the given class so that it can be retrieved
+    based on its kind.
+    """
+    obj = cls("")
+    global kind_to_error_type
+    kind_to_error_type[obj.kind] = cls
+    return cls
+
+
 class BaseAPIError(BaseException):
     """All API errors subclass this error."""
     def __init__(self, kind, description):
@@ -24,97 +34,77 @@ class BaseAPIError(BaseException):
         return s3
 
 
+@register_error_type
 class UnknownError(BaseAPIError):
     """Something unexpected happened. The server may be in an invalid state."""
     def __init__(self, description):
         super().__init__(error_kinds.UNKNOWN, description)
 
 
-kind_to_error_type[error_kinds.UNKNOWN] = UnknownError
-
-
+@register_error_type
 class StreamConfigNotFoundError(BaseAPIError):
     """A StreamConfiguration specified by the client could not be found."""
     def __init__(self, description):
         super().__init__(error_kinds.STREAM_CONFIG_NOT_FOUND, description)
 
 
-kind_to_error_type[error_kinds.STREAM_CONFIG_NOT_FOUND] = \
-    StreamConfigNotFoundError
-
-
+@register_error_type
 class ZoneNotFoundError(BaseAPIError):
     """A Zone specified by the client could not be found."""
     def __init__(self, description):
         super().__init__(error_kinds.ZONE_NOT_FOUND, description)
 
 
-kind_to_error_type[error_kinds.ZONE_NOT_FOUND] = ZoneNotFoundError
-
-
+@register_error_type
 class ZoneNotDeletableError(BaseAPIError):
     """A client tried to delete a default Zone"""
     def __init__(self, description):
         super().__init__(error_kinds.ZONE_NOT_DELETABLE, description)
 
 
-kind_to_error_type[error_kinds.ZONE_NOT_DELETABLE] = ZoneNotDeletableError
-
-
+@register_error_type
 class AlertNotFoundError(BaseAPIError):
     """An Alert specified by the client could not be found."""
     def __init__(self, description):
         super().__init__(error_kinds.ALERT_NOT_FOUND, description)
 
 
-kind_to_error_type[error_kinds.ALERT_NOT_FOUND] = AlertNotFoundError
-
-
+@register_error_type
 class StreamNotFoundError(BaseAPIError):
     """The corresponding stream for a StreamConfiguration is not available."""
     def __init__(self, description):
         super().__init__(error_kinds.STREAM_NOT_FOUND, description)
 
 
-kind_to_error_type[error_kinds.STREAM_NOT_FOUND] = StreamNotFoundError
-
-
+@register_error_type
 class InvalidSyntaxError(BaseAPIError):
     """The syntax of the request could not be parsed."""
     def __init__(self, description):
         super().__init__(error_kinds.INVALID_SYNTAX, description)
 
 
-kind_to_error_type[error_kinds.INVALID_SYNTAX] = InvalidSyntaxError
-
-
+@register_error_type
 class InvalidFormatError(BaseAPIError):
     """The request was parsed, but some value within the request is invalid."""
     def __init__(self, description):
         super().__init__(error_kinds.INVALID_FORMAT, description)
 
 
-kind_to_error_type[error_kinds.INVALID_FORMAT] = InvalidFormatError
-
-
+@register_error_type
 class NotImplementedInAPIError(BaseAPIError):
     """The client requested something that is not currently implemented."""
     def __init__(self, description):
         super().__init__(error_kinds.NOT_IMPLEMENTED, description)
 
 
-kind_to_error_type[error_kinds.NOT_IMPLEMENTED] = NotImplementedInAPIError
-
-
+@register_error_type
 class StreamNotOpenedError(BaseAPIError):
     """A stream failed to open when it was required to."""
     def __init__(self, description):
         super().__init__(error_kinds.STREAM_NOT_OPENED, description)
 
 
-kind_to_error_type[error_kinds.STREAM_NOT_OPENED] = StreamNotOpenedError
-
-
+@register_error_type
 class DuplicateStreamSourceError(BaseAPIError):
     """There was an attempted to create a stream configuration with the same
     source as an existing one.
@@ -123,10 +113,7 @@ class DuplicateStreamSourceError(BaseAPIError):
         super().__init__(error_kinds.DUPLICATE_STREAM_SOURCE, description)
 
 
-kind_to_error_type[error_kinds.DUPLICATE_STREAM_SOURCE] = \
-    DuplicateStreamSourceError
-
-
+@register_error_type
 class DuplicateZoneNameError(BaseAPIError):
     """There was an attempt to make a zone with the same name as another zone
     within the same stream.
@@ -135,9 +122,7 @@ class DuplicateZoneNameError(BaseAPIError):
         super().__init__(error_kinds.DUPLICATE_ZONE_NAME, description)
 
 
-kind_to_error_type[error_kinds.DUPLICATE_ZONE_NAME] = DuplicateZoneNameError
-
-
+@register_error_type
 class DuplicateIdentityNameError(BaseAPIError):
     """There was an attempt to create a new identity with the same name as
     another identity.
@@ -146,39 +131,32 @@ class DuplicateIdentityNameError(BaseAPIError):
         super().__init__(error_kinds.DUPLICATE_IDENTITY_NAME, description)
 
 
-kind_to_error_type[error_kinds.DUPLICATE_IDENTITY_NAME] = \
-    DuplicateIdentityNameError
-
-
+@register_error_type
 class NotDetectableError(BaseAPIError):
-    """There was an attempt to use a class name that is not detectable."""
+    """There was an attempt to use a @register_error_type
+class name that is not detectable."""
     def __init__(self, description):
         super().__init__(error_kinds.NOT_DETECTABLE, description)
 
 
-kind_to_error_type[error_kinds.NOT_DETECTABLE] = NotDetectableError
-
-
+@register_error_type
 class NoEncoderForClassError(BaseAPIError):
-    """There was an attempt to create an identity for a class that is not
+    """There was an attempt to create an identity for a @register_error_type
+class that is not
     encodable.
     """
     def __init__(self, description):
         super().__init__(error_kinds.NO_ENCODER_FOR_CLASS, description)
 
 
-kind_to_error_type[error_kinds.NO_ENCODER_FOR_CLASS] = NoEncoderForClassError
-
-
+@register_error_type
 class IdentityNotFoundError(BaseAPIError):
     """An identity specified by the client could not be found."""
     def __init__(self, description):
         super().__init__(error_kinds.IDENTITY_NOT_FOUND, description)
 
 
-kind_to_error_type[error_kinds.IDENTITY_NOT_FOUND] = IdentityNotFoundError
-
-
+@register_error_type
 class ImageNotFoundForIdentityError(BaseAPIError):
     """An image specified by the client could not be found for the specified
     identity.
@@ -187,19 +165,14 @@ class ImageNotFoundForIdentityError(BaseAPIError):
         super().__init__(error_kinds.IMAGE_NOT_FOUND_FOR_IDENTITY, description)
 
 
-kind_to_error_type[error_kinds.IMAGE_NOT_FOUND_FOR_IDENTITY] = \
-    ImageNotFoundForIdentityError
-
-
+@register_error_type
 class InvalidImageTypeError(BaseAPIError):
     """An image could not be decoded by OpenCV"""
     def __init__(self, description):
         super().__init__(error_kinds.INVALID_IMAGE_TYPE, description)
 
 
-kind_to_error_type[error_kinds.INVALID_IMAGE_TYPE] = InvalidImageTypeError
-
-
+@register_error_type
 class AnalysisLimitExceededError(BaseAPIError):
     """There was an attempt to start analysis on a stream, but the maximum
     amount of streams that may have analysis run on them at once has already
@@ -209,34 +182,27 @@ class AnalysisLimitExceededError(BaseAPIError):
         super().__init__(error_kinds.ANALYSIS_LIMIT_EXCEEDED, description)
 
 
-kind_to_error_type[error_kinds.ANALYSIS_LIMIT_EXCEEDED] = \
-    AnalysisLimitExceededError
-
-
+@register_error_type
 class NoDetectionsInImageError(BaseAPIError):
     """There was an attempt to encode an image with no objects of the given
-    class in the frame.
+    @register_error_type
+class in the frame.
     """
     def __init__(self, description):
         super().__init__(error_kinds.NO_DETECTIONS_IN_IMAGE, description)
 
 
-kind_to_error_type[error_kinds.NO_DETECTIONS_IN_IMAGE] = \
-    NoDetectionsInImageError
-
-
+@register_error_type
 class TooManyDetectionsInImageError(BaseAPIError):
     """There was an attempt to encode an image with more than one object of the
-    given class in the frame, causing ambiguity on which one to encode.
+    given @register_error_type
+class in the frame, causing ambiguity on which one to encode.
     """
     def __init__(self, description):
         super().__init__(error_kinds.TOO_MANY_DETECTIONS_IN_IMAGE, description)
 
 
-kind_to_error_type[error_kinds.TOO_MANY_DETECTIONS_IN_IMAGE] = \
-    TooManyDetectionsInImageError
-
-
+@register_error_type
 class ImageAlreadyEncodedError(BaseAPIError):
     """There was an attempt to encode an image that has already been encoded for
     a given identity and a given class.
@@ -245,9 +211,7 @@ class ImageAlreadyEncodedError(BaseAPIError):
         super().__init__(error_kinds.IMAGE_ALREADY_ENCODED, description)
 
 
-kind_to_error_type[error_kinds.IMAGE_ALREADY_ENCODED] = ImageAlreadyEncodedError
-
-
+@register_error_type
 class DuplicateVectorError(BaseAPIError):
     """There was an attempt to add a vector that already exists for the given
     identity and class.
@@ -256,14 +220,8 @@ class DuplicateVectorError(BaseAPIError):
         super().__init__(error_kinds.DUPLICATE_VECTOR, description)
 
 
-kind_to_error_type[error_kinds.DUPLICATE_VECTOR] = DuplicateVectorError
-
-
+@register_error_type
 class FrameNotFoundForAlertError(BaseAPIError):
     """There was an attempt to get a frame for an alert that has no frame."""
     def __init__(self, description):
         super().__init__(error_kinds.FRAME_NOT_FOUND_FOR_ALERT, description)
-
-
-kind_to_error_type[error_kinds.FRAME_NOT_FOUND_FOR_ALERT] = \
-    FrameNotFoundForAlertError
