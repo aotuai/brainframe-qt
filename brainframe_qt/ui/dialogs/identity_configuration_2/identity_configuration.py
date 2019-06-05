350155c09913508534cf18fb9bfa084c66aa1156
Bryce Beagle <bryce.beagle@dilililabs.com>
2019-06-05 16:38:01 -0700

    Start of theming

--- brainframe_qt/ui/dialogs/identity_configuration_2/identity_configuration.py
+++ brainframe_qt/ui/dialogs/identity_configuration_2/identity_configuration.py
@@ -1,11 +1,13 @@
 from PyQt5.QtCore import pyqtSlot
-from PyQt5.QtWidgets import QDialog
+from PyQt5.QtGui import QPalette
+from PyQt5.QtWidgets import QDialog, QLineEdit
 from PyQt5.uic import loadUi
 
 from brainframe.client.api.codecs import Identity
 from brainframe.client.ui.resources.paths import qt_ui_paths
 
 from .identity_info import IdentityInfo
+from .identity_search_filter import IdentitySearchFilter
 
 
 class IdentityConfiguration(QDialog):
@@ -15,12 +17,40 @@ class IdentityConfiguration(QDialog):
         loadUi(qt_ui_paths.identity_configuration_ui2, self)
 
         self.identity_info: IdentityInfo
-        self.identity_info.hide()
+        self.identity_search_filter: IdentitySearchFilter
+
+        self.init_ui()
 
     @classmethod
     def show_dialog(cls):
         dialog = cls()
         dialog.exec_()
+
+    def init_ui(self):
+        self.identity_info: IdentityInfo
+        self.identity_search_filter: IdentitySearchFilter
+
+        self.identity_info.hide()
+        self.init_theming()
+
+    def init_theming(self):
+        """Change color palettes"""
+
+        palette = self.identity_search_filter.palette()
+        palette.setColor(QPalette.Window, palette.alternateBase().color())
+        self.identity_search_filter.setPalette(palette)
+        self.identity_search_filter.setAutoFillBackground(True)
+
+        line_edit: QLineEdit = self.identity_search_filter.search_line_edit
+        palette = line_edit.palette()
+        palette.setColor(QPalette.Base, self.palette().window().color())
+        line_edit.setPalette(palette)
+        line_edit.setAutoFillBackground(True)
+
+        palette = self.identity_info.palette()
+        palette.setColor(QPalette.Window, palette.alternateBase().color())
+        self.identity_info.setPalette(palette)
+        self.identity_info.setAutoFillBackground(True)
 
     @pyqtSlot(object)
     def show_identity_info_slot(self, identity: Identity):
