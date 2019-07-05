f536a1ebb9ee354017d433f8e7ba2855de29a071
Bryce Beagle <bryce.beagle@dilililabs.com>
2019-07-05 16:50:17 -0700

    Fix margin issues. Fix transparency with scroll area

--- brainframe_qt/ui/main_window/video_thumbnail_view/thumbnail_scroll_area/thumbnail_scroll_area.py
+++ brainframe_qt/ui/main_window/video_thumbnail_view/thumbnail_scroll_area/thumbnail_scroll_area.py
@@ -1,13 +1,32 @@
-import sys
-
-from PyQt5.QtCore import Qt, QPoint, QRect, QSize
-from PyQt5.QtWidgets import QScrollArea, QSizePolicy, QStyle, QWidget
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QScrollArea, QWidget
 
 
 class ThumbnailScrollArea(QScrollArea):
     """ScrollArea that accounts for width of the scrollbars when calculating
     size
     """
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self._init_ui()
+
+    def _init_ui(self):
+        # Make this widget transparent
+        palette = self.palette()
+        palette.setColor(palette.Window, Qt.transparent)
+        self.setPalette(palette)
+
+    def setWidget(self, widget: QWidget) -> None:
+        """Force the inner container widget to be transparent. For whatever
+        reason, it's not transparent by default. Using stylesheet doesn't work
+        because it affects children"""
+
+        super().setWidget(widget)
+
+        palette = widget.palette()
+        palette.setColor(palette.Window, Qt.transparent)
+        widget.setPalette(palette)
 
     def resizeEvent(self, event):
 
