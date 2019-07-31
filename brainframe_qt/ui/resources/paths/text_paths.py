40cf4e974468723391127debdd7639c6a93a7266
alex <alex.thiel@dilililabs.com>
2019-07-31 15:00:44 -0700

    Fixed route_path bug

--- brainframe_qt/ui/resources/paths/text_paths.py
+++ brainframe_qt/ui/resources/paths/text_paths.py
@@ -1,7 +1,7 @@
 from .all_resource_paths import route_path
 
 # Translation paths
-i18n_dir = route_path("brainframe") / "client" / "ui" / "resources" / "i18n"
+i18n_dir = route_path("brainframe", "client", "ui", "resources", "i18n")
 
 # License paths
 license_agreement_txt = route_path("license.txt")
