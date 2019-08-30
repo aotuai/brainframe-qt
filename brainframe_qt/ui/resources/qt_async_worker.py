2a2ff878123d252e0c21339b3d3ff0fd95823645
Bryce Beagle <bryce.beagle@dilililabs.com>
2019-08-30 09:41:26 -0700

    Don't use threads in QtDesigner

--- brainframe_qt/ui/resources/qt_async_worker.py
+++ brainframe_qt/ui/resources/qt_async_worker.py
@@ -1,3 +1,5 @@
+import os
+
 from PyQt5.QtCore import QThread
 
 
@@ -22,3 +24,15 @@ class QTAsyncWorker(QThread):
 
     def run(self):
         self.result = self.func(*self.args, **self.kwargs)
+
+    def start(self, *args, **kwargs):
+        # We don't want to use threads when using QtDesigner. If a plugin's
+        # __init__ contains a QThread that is not complete by the time the
+        # __init__ method finishes, it will crash
+        # This environment variable is only set when running QtDesigner
+        if os.getenv("PYQTDESIGNERPATH") is not None:
+            self.run()
+            # noinspection PyUnresolvedReferences
+            self.finished.emit()
+        else:
+            super().start(*args, **kwargs)
