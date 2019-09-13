736211a32116e9d864a6dc3baaa02b81a4a87545
Tyler Compton <tyler.compton@dilililabs.com>
2019-09-13 11:40:33 -0700

    Move intersection point field over to individual condition classes
    (cherry picked from commit 2956147ccfa55a57a51e6b0c7251f2951022e005)

--- brainframe_qt/api/codecs/__init__.py
+++ brainframe_qt/api/codecs/__init__.py
@@ -1,7 +1,8 @@
 from .base_codecs import Codec
 
 from .alarm_codecs import Alert, ZoneAlarm
-from .condition_codecs import ZoneAlarmCountCondition, ZoneAlarmRateCondition
+from .condition_codecs import ZoneAlarmCountCondition, ZoneAlarmRateCondition,\
+    IntersectionPointType
 from .config_codecs import EngineConfiguration, StreamConfiguration
 from .detection_codecs import Attribute, Detection, Identity
 from .zone_codecs import Zone, ZoneAlarm, ZoneStatus
--- brainframe_qt/api/codecs/condition_codecs.py
+++ brainframe_qt/api/codecs/condition_codecs.py
@@ -10,6 +10,9 @@ condition_test_map = {'>': "Greater than",
                       "=": "Exactly"}
 
 
+IntersectionPointType = codec_enums.IntersectionPointType
+
+
 class ZoneAlarmCountCondition(Codec):
     """A condition that must be met for an alarm to go off. Compares the number
     of objects in a zone to some number.
@@ -18,11 +21,12 @@ class ZoneAlarmCountCondition(Codec):
     TestType = codec_enums.CountConditionTestType
 
     def __init__(self, *, test, check_value, with_class_name, with_attribute,
-                 id_=None):
+                 intersection_point, id_=None):
         self.test = test
         self.check_value = check_value
         self.with_class_name = with_class_name
         self.with_attribute = with_attribute
+        self.intersection_point = intersection_point
         self.id = id_
 
     def __repr__(self):
@@ -34,6 +38,7 @@ class ZoneAlarmCountCondition(Codec):
 
     def to_dict(self) -> dict:
         d = dict(self.__dict__)
+        d["intersection_point"] = self.intersection_point.value
         if self.with_attribute is not None:
             d["with_attribute"] = self.with_attribute.to_dict()
 
@@ -41,6 +46,8 @@ class ZoneAlarmCountCondition(Codec):
 
     @staticmethod
     def from_dict(d: dict):
+        intersection_point = IntersectionPointType(
+            d["intersection_point"])
         # with_attribute is an optional parameter
         with_attribute = None
         if d["with_attribute"] is not None:
@@ -51,6 +58,7 @@ class ZoneAlarmCountCondition(Codec):
             check_value=d["check_value"],
             with_class_name=d["with_class_name"],
             with_attribute=with_attribute,
+            intersection_point=intersection_point,
             id_=d["id"])
 
 
@@ -60,7 +68,6 @@ class ZoneAlarmRateCondition(Codec):
     """
 
     TestType = codec_enums.RateConditionTestType
-
     DirectionType = codec_enums.DirectionType
 
     direction_map = {DirectionType.ENTERING: "entered",
@@ -68,13 +75,14 @@ class ZoneAlarmRateCondition(Codec):
                      DirectionType.ENTERING_OR_EXITING: "entered or exited"}
 
     def __init__(self, *, test, duration, change, direction, with_class_name,
-                 with_attribute, id_=None):
+                 with_attribute, intersection_point, id_=None):
         self.test = test
         self.duration = duration
         self.change = change
         self.direction = direction
         self.with_class_name = with_class_name
         self.with_attribute = with_attribute
+        self.intersection_point = intersection_point
         self.id = id_
 
     def __repr__(self):
@@ -85,6 +93,7 @@ class ZoneAlarmRateCondition(Codec):
 
     def to_dict(self) -> dict:
         d = dict(self.__dict__)
+        d["intersection_point"] = self.intersection_point.value
         if self.with_attribute is not None:
             d["with_attribute"] = self.with_attribute.to_dict()
 
@@ -94,6 +103,8 @@ class ZoneAlarmRateCondition(Codec):
 
     @staticmethod
     def from_dict(d: dict):
+        intersection_point = IntersectionPointType(
+            d["intersection_point"])
         # with_attribute is an optional parameter
         with_attribute = None
         if d["with_attribute"] is not None:
@@ -106,5 +117,6 @@ class ZoneAlarmRateCondition(Codec):
             direction=ZoneAlarmRateCondition.DirectionType(d["direction"]),
             with_class_name=d["with_class_name"],
             with_attribute=with_attribute,
+            intersection_point=intersection_point,
             id_=d["id"])
 
