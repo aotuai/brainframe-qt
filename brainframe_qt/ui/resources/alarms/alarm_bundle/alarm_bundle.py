885afb163ba7f5666a5c7c8e5d35599dbce0609d
Bryce Beagle <bryce.beagle@dilililabs.com>
2020-02-14 12:45:41 -0800

    Collapse bundle and children when clicked

--- brainframe_qt/ui/resources/alarms/alarm_bundle/alarm_bundle.py
+++ brainframe_qt/ui/resources/alarms/alarm_bundle/alarm_bundle.py
@@ -1,12 +1,14 @@
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QWidget, QVBoxLayout, QFrame
+from PyQt5.QtWidgets import QFrame, QVBoxLayout, QWidget, QLayout
 
 from brainframe.client.ui.resources import stylesheet_watcher
-from brainframe.client.ui.resources.alarms.alarm_bundle.bundle_header import \
-    BundleHeader
-from brainframe.client.ui.resources.paths import qt_qss_paths
 from brainframe.client.ui.resources.alarms.alarm_bundle.alarm_card \
     import AlarmCard
+from brainframe.client.ui.resources.alarms.alarm_bundle.bundle_header import \
+    BundleHeader
+from brainframe.client.ui.resources.mixins.data_structure import IterableMI
+from brainframe.client.ui.resources.mixins.display import ExpandableMI
+from brainframe.client.ui.resources.paths import qt_qss_paths
 
 
 class AlarmBundleUI(QWidget):
@@ -51,12 +53,33 @@ class AlarmBundleUI(QWidget):
         return bundle_header
 
 
-class AlarmBundle(AlarmBundleUI):
+class AlarmBundle(AlarmBundleUI, ExpandableMI, IterableMI):
     def __init__(self, parent: QWidget):
         super().__init__(parent)
+        self._init_signals()
 
         for _ in range(3):
             self.add_alarm_card()
+
+    def _init_signals(self):
+        self.bundle_header.clicked.connect(self.toggle_expansion)
+
+    def expansion_changed(self):
+
+        # noinspection PyPropertyAccess
+        now_expanded = self.expanded
+
+        # Collapse all AlarmCards before collapsing the bundle
+        if not now_expanded:
+            for alarm_card in self:
+                alarm_card: AlarmCard
+                alarm_card.expanded = False
+
+        self.alarm_container.setVisible(now_expanded)
+        stylesheet_watcher.update()
+
+    def iterable_layout(self) -> QLayout:
+        return self.alarm_container.layout()
 
     def add_alarm_card(self):
         self.alarm_container.layout().addWidget(AlarmCard(self))
