ca3d4805688f00b88c1f35e8e32843fffa46bf15
Bryce Beagle <bryce.beagle@dilililabs.com>
2020-02-14 10:03:34 -0800

    Add a bundle header and put the alarms in a layout

--- brainframe_qt/ui/resources/alarms/alarm_bundle/alarm_bundle.py
+++ brainframe_qt/ui/resources/alarms/alarm_bundle/alarm_bundle.py
@@ -1,7 +1,9 @@
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QWidget, QVBoxLayout
+from PyQt5.QtWidgets import QWidget, QVBoxLayout, QFrame
 
 from brainframe.client.ui.resources import stylesheet_watcher
+from brainframe.client.ui.resources.alarms.alarm_bundle.bundle_header import \
+    BundleHeader
 from brainframe.client.ui.resources.paths import qt_qss_paths
 from brainframe.client.ui.resources.alarms.alarm_bundle.alarm_card \
     import AlarmCard
@@ -12,11 +14,19 @@ class AlarmBundleUI(QWidget):
     def __init__(self, parent: QWidget):
         super().__init__(parent)
 
+        self.bundle_header = self._init_bundle_header()
+        self.alarm_container = self._init_alarm_container()
+
         self._init_layout()
         self._init_style()
 
     def _init_layout(self) -> None:
         layout = QVBoxLayout()
+        layout.setContentsMargins(0, 0, 0, 0)
+
+        layout.addWidget(self.bundle_header)
+        layout.addWidget(self.alarm_container)
+
         self.setLayout(layout)
 
     def _init_style(self):
@@ -24,6 +34,16 @@ class AlarmBundleUI(QWidget):
         self.setAttribute(Qt.WA_StyledBackground, True)
 
         stylesheet_watcher.watch(self, qt_qss_paths.alarm_bundle_qss)
+
+    def _init_alarm_container(self) -> QWidget:
+        alarm_container = QFrame(self)
+        alarm_container.setObjectName("alarm_container")
+        alarm_container.setLayout(QVBoxLayout())
+        return alarm_container
+
+    def _init_bundle_header(self) -> QWidget:
+        bundle_header = BundleHeader(self)
+        return bundle_header
 
 
 class AlarmBundle(AlarmBundleUI):
@@ -34,7 +54,7 @@ class AlarmBundle(AlarmBundleUI):
             self.add_alarm_card()
 
     def add_alarm_card(self):
-        self.layout().addWidget(AlarmCard(self))
+        self.alarm_container.layout().addWidget(AlarmCard(self))
 
 
 if __name__ == '__main__':
@@ -47,7 +67,7 @@ if __name__ == '__main__':
     window = QWidget()
     window.setAttribute(Qt.WA_StyledBackground, True)
 
-    window.setLayout(QVBoxLayout(window))
+    window.setLayout(QVBoxLayout())
     window.layout().addWidget(AlarmBundle(typing.cast(QWidget, None)))
 
     window.show()
