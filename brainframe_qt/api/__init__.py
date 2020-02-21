5c1c4736ea537f5b821f2cfc073eb5ae002a9745
Bryce Beagle <bryce.beagle@dilililabs.com>
2020-02-21 09:10:01 -0800

    Add mock for get_zone_alarms

--- brainframe_qt/api/__init__.py
+++ brainframe_qt/api/__init__.py
@@ -36,6 +36,8 @@ class APIWrapper(API):
             return self._empty_func([])
         if item == "get_encoding_class_names":
             return self._empty_func([])
+        if item == "get_zone_alarms":
+            return self._empty_func([])
 
         return None
 
