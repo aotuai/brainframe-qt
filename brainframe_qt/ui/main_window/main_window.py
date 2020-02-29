812a064b8d919d8e5e101ec6b9cf7c576362adc0
alex <alex.thiel@aotu.ai>
2020-02-28 17:14:21 -0800

    Removed erroneous prints

--- brainframe_qt/ui/main_window/main_window.py
+++ brainframe_qt/ui/main_window/main_window.py
@@ -19,14 +19,8 @@ class MainWindow(QMainWindow):
 
     def __init__(self, parent=None):
         super().__init__(parent)
-        import brainframe.client.ui.main_window.video_thumbnail_view.video_thumbnail_view as test
-        print("Ayy lmao", test, test.__file__)
-        import os
-        print("plugins path", os.getenv("QT_PLUGIN_PATH"))
-        print("gunna do UI. ", qt_ui_paths.main_window_ui)
-        ui = loadUi(qt_ui_paths.main_window_ui, self)
-        print("UI got load. ", ui)
-        ui.show()
+
+        loadUi(qt_ui_paths.main_window_ui, self).show()
 
         self._init_ui()
 
