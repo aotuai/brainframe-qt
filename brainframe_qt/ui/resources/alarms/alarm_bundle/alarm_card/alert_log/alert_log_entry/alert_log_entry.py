3d63a35fe3ea6933754ade5b12e305f6a3bc4ced
Bryce Beagle <bryce.beagle@dilililabs.com>
2020-03-10 11:26:29 -0700

    Add an alert end time label and remove the description, active/inactive states for now

--- brainframe_qt/ui/resources/alarms/alarm_bundle/alarm_card/alert_log/alert_log_entry/alert_log_entry.py
+++ brainframe_qt/ui/resources/alarms/alarm_bundle/alarm_card/alert_log/alert_log_entry/alert_log_entry.py
@@ -1,7 +1,9 @@
+import pendulum
+from cached_property import cached_property
 from typing import Optional
 
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QWidget, QLabel, QHBoxLayout, QSizePolicy, QFrame
+from PyQt5.QtWidgets import QFrame, QHBoxLayout, QLabel, QSizePolicy, QWidget
 
 from brainframe.client.api.codecs import Alert
 from brainframe.client.ui.resources import stylesheet_watcher
@@ -10,12 +12,14 @@ from brainframe.client.ui.resources.paths import qt_qss_paths
 
 class AlertLogEntryUI(QFrame):
 
-    def __init__(self, parent: Optional[QWidget]):
+    def __init__(self, parent: QWidget):
         super().__init__(parent)
 
-        self.time_label = self._init_time_label()
-        self.active_state_label = self._init_active_state_label()
-        self.alert_description_label = self._init_alert_description_label()
+        self.start_time_label = self._init_start_time_label()
+        self.time_dash_label = self._init_time_dash_label()
+        self.end_time_label = self._init_end_time_label()
+        # self.active_state_label = self._init_active_state_label()
+        # self.alert_description_label = self._init_alert_description_label()
 
         self._init_layout()
         self._init_style()
@@ -23,9 +27,12 @@ class AlertLogEntryUI(QFrame):
     def _init_layout(self) -> None:
         layout = QHBoxLayout()
 
-        layout.addWidget(self.time_label)
-        layout.addWidget(self.active_state_label)
-        layout.addWidget(self.alert_description_label)
+        layout.addWidget(self.start_time_label)
+        layout.addWidget(self.time_dash_label)
+        layout.addWidget(self.end_time_label)
+        layout.addStretch()
+        # layout.addWidget(self.active_state_label)
+        # layout.addWidget(self.alert_description_label)
 
         self.setLayout(layout)
 
@@ -35,34 +42,54 @@ class AlertLogEntryUI(QFrame):
 
         stylesheet_watcher.watch(self, qt_qss_paths.alert_log_entry_qss)
 
-    def _init_time_label(self) -> QLabel:
-        time_label = QLabel("13:37", self)
-        time_label.setObjectName("time")
+    def _init_start_time_label(self) -> QLabel:
+        start_time_label = QLabel("00:00", self)
+        start_time_label.setObjectName("start_time")
 
-        time_label.setFixedWidth(self._text_width("22:22"))
+        start_time_label.setFixedWidth(self._max_time_width)
 
-        time_label.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Fixed)
-        return time_label
+        start_time_label.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
+        return start_time_label
 
-    def _init_active_state_label(self) -> QLabel:
-        active_state_label = QLabel("Inactive", self)
-        active_state_label.setObjectName("active_state")
+    def _init_time_dash_label(self) -> QLabel:
+        time_dash_label = QLabel("–", self)
+        time_dash_label.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
+        return time_dash_label
 
-        active_state_label.setFixedWidth(self._text_width("Inactive"))
+    def _init_end_time_label(self) -> QLabel:
+        end_time_label = QLabel("00:00", self)
+        end_time_label.setObjectName("end_time")
 
-        active_state_label.setSizePolicy(QSizePolicy.Preferred,
-                                         QSizePolicy.Fixed)
-        return active_state_label
+        end_time_label.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
+        return end_time_label
 
-    def _init_alert_description_label(self) -> QLabel:
-        alert_description_label = QLabel("Tyler has smelly feet", self)
-        alert_description_label.setObjectName("alert_description")
-        alert_description_label.setSizePolicy(QSizePolicy.Expanding,
-                                              QSizePolicy.Maximum)
-        return alert_description_label
+    # def _init_active_state_label(self) -> QLabel:
+    #     active_state_label = QLabel("[Alert Activeness]", self)
+    #     active_state_label.setObjectName("active_state")
+    #
+    #     active_state_label.setFixedWidth(self._text_width("Inactive"))
+    #
+    #     active_state_label.setSizePolicy(QSizePolicy.Preferred,
+    #                                      QSizePolicy.Fixed)
+    #     return active_state_label
+    #
+    # def _init_alert_description_label(self) -> QLabel:
+    #     alert_description_label = QLabel("[Alert Description]", self)
+    #     alert_description_label.setObjectName("alert_description")
+    #     alert_description_label.setSizePolicy(QSizePolicy.Expanding,
+    #                                           QSizePolicy.Maximum)
+    #     return alert_description_label
 
-    def _text_width(self, string: str) -> int:
-        return self.fontMetrics().boundingRect(string).width()
+    def _text_width(self, text: str) -> int:
+        # https://stackoverflow.com/a/32078341/8134178
+        initial_rect = self.fontMetrics().boundingRect(text)
+        improved_rect = self.fontMetrics().boundingRect(initial_rect, 0, text)
+        return improved_rect.width()
+
+    @cached_property
+    def _max_time_width(self):
+        return max(self._text_width(f"{0}{0}:{0}{0}".format(n))
+                   for n in range(10))
 
 
 class AlertLogEntry(AlertLogEntryUI):
@@ -70,3 +97,26 @@ class AlertLogEntry(AlertLogEntryUI):
     def __init__(self, alert: Alert, parent: QWidget):
         super().__init__(parent)
 
+        self.alert = alert
+
+        self._populate_alert_info()
+
+    def _populate_alert_info(self) -> None:
+        # Alert start time
+        start_time_str = self._unix_time_to_tz_time(self.alert.start_time)
+        self.start_time_label.setText(start_time_str)
+
+        # Alert end time
+        end_time_str = self._unix_time_to_tz_time(self.alert.end_time) \
+            if self.alert.end_time \
+            else ""
+        self.end_time_label.setText(end_time_str)
+
+        # # Active state
+        # active_state_str = "Inactive" if alert.end_time else "Active"
+        # self.active_state_label.setText(active_state_str)
+
+    def _unix_time_to_tz_time(self, timestamp: float) -> str:
+        timezone = "America/Los_Angeles"
+        time = pendulum.from_timestamp(timestamp, tz=timezone)
+        return time.strftime("%I:%M")  # Maybe use `zz` too?
