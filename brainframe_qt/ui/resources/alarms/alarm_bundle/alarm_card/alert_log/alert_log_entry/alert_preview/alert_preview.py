b6bda728229c9dfed227eef876ca734360b7ab47
Bryce Beagle <bryce.beagle@dilililabs.com>
2020-03-15 21:51:04 -0700

    Add image for alert to the AlarmPreview widget

--- brainframe_qt/ui/resources/alarms/alarm_bundle/alarm_card/alert_log/alert_log_entry/alert_preview/alert_preview.py
+++ brainframe_qt/ui/resources/alarms/alarm_bundle/alarm_card/alert_log/alert_log_entry/alert_preview/alert_preview.py
@@ -1,9 +1,18 @@
-from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QFrame, QWidget, QVBoxLayout, QGraphicsView
+import logging
+from typing import Optional
 
+import numpy as np
+
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QPixmap, QImage
+from PyQt5.QtWidgets import QFrame, QWidget, QVBoxLayout, QGraphicsPixmapItem
+
+from brainframe.client.api import api
 from brainframe.client.api.codecs import Alert
-from brainframe.client.ui.resources import stylesheet_watcher
+from brainframe.client.ui.resources import stylesheet_watcher, QTAsyncWorker
 from brainframe.client.ui.resources.paths import qt_qss_paths
+
+from .alert_preview_view import AlertPreviewView
 
 
 class AlertPreviewUI(QFrame):
@@ -12,21 +21,28 @@ class AlertPreviewUI(QFrame):
 
         self.alert = alert
 
-        self.alert_image = self._init_alert_image()
+        self.image_item = self._init_image_item()
+        self.alert_image_view = self._init_alert_image_view()
 
         self._init_layout()
         self._init_style()
 
-    def _init_alert_image(self) -> QGraphicsView:
-        alert_header = QGraphicsView(self)
+    def _init_image_item(self) -> QGraphicsPixmapItem:
+        image_item = QGraphicsPixmapItem(QPixmap())
 
-        return alert_header
+        return image_item
+
+    def _init_alert_image_view(self) -> AlertPreviewView:
+        alert_image_view = AlertPreviewView(self)
+        alert_image_view.scene().addItem(self.image_item)
+
+        return alert_image_view
 
     def _init_layout(self) -> None:
         layout = QVBoxLayout()
         layout.setContentsMargins(0, 0, 0, 0)
 
-        layout.addWidget(self.alert_image)
+        layout.addWidget(self.alert_image_view)
 
         self.setLayout(layout)
 
@@ -37,8 +53,46 @@ class AlertPreviewUI(QFrame):
 
         stylesheet_watcher.watch(self, qt_qss_paths.alert_preview_qss)
 
+    def resizeEvent(self, event=None):
+        """Take up entire width using aspect ratio of scene"""
+
+        bounding_rect = self.image_item.boundingRect()
+        self.alert_image_view.scene().setSceneRect(bounding_rect)
+        self.alert_image_view.fitInView(bounding_rect, Qt.KeepAspectRatio)
+
 
 class AlertPreview(AlertPreviewUI):
 
     def __init__(self, alert: Alert, parent: QWidget):
         super().__init__(alert, parent)
+
+        self.load_alert_image()
+
+    def load_alert_image(self):
+
+        def handle_frame(frame: Optional[np.ndarray]):
+            if frame is None:
+                # TODO: Handle a missing frame
+                logging.warning("TODO: No frame for alert found")
+                return
+
+            image = self._ndarray_to_qimage(frame)
+            self.set_image(image)
+
+        QTAsyncWorker(self, api.get_alert_frame, f_args=(self.alert.id,),
+                      on_success=handle_frame) \
+            .start()
+
+    def set_image(self, image: QImage):
+        self.image_item.setPixmap(QPixmap(image))
+        self.alert_image_view.fitInView(self.image_item.boundingRect(),
+                                        Qt.KeepAspectRatio)
+
+    @staticmethod
+    def _ndarray_to_qimage(array: np.ndarray) -> QImage:
+        height, width, channels = array.shape
+        bytesPerLine = channels * width
+        image = QImage(array.data, width, height, bytesPerLine,
+                       QImage.Format_RGB888).rgbSwapped()
+
+        return image
