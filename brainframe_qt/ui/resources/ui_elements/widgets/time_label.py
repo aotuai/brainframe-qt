da8010d6621e6f1980243a76ea149f6fb1f8d117
Bryce Beagle <bryce.beagle@dilililabs.com>
2020-04-10 16:11:28 -0700

    Fix issue where _max_time_width was missing @property decorator

--- brainframe_qt/ui/resources/ui_elements/widgets/time_label.py
+++ brainframe_qt/ui/resources/ui_elements/widgets/time_label.py
@@ -46,6 +46,7 @@ class TimeLabelUI(QWidget):
         improved_rect = self.fontMetrics().boundingRect(initial_rect, 0, text)
         return improved_rect.width()
 
+    @property
     def _max_time_width(self):
         return max(self._text_width(f"{0}{0}:{0}{0}".format(n))
                    for n in range(10))
