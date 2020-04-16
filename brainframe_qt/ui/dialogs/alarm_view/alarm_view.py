792620f8d09d10fe95a70ac39148d406ff241bc8
Bryce Beagle <bryce.beagle@aotu.ai>
2020-04-16 16:37:02 -0700

    (WIP) Splitting alarm_view.py into two files, one with UI
    https://stackoverflow.com/a/53849651/8134178

--- brainframe_qt/ui/dialogs/alarm_view/alarm_view.py
+++ brainframe_qt/ui/dialogs/alarm_view/alarm_view.py
@@ -1,65 +1,15 @@
 from typing import Dict, List, Union
 
-from PyQt5.QtCore import QMargins, Qt, pyqtSlot, QThread, QMetaObject, Q_ARG
-from PyQt5.QtWidgets import QApplication, QLayout, QScrollArea, QVBoxLayout, \
-    QWidget, QDialog
+from PyQt5.QtCore import QMetaObject, QThread, Q_ARG, Qt, pyqtSlot
+from PyQt5.QtWidgets import QApplication, QDialog, QLayout, QVBoxLayout, \
+    QWidget
 
 from brainframe.client.api import api
 from brainframe.client.api.codecs import StreamConfiguration, Zone, ZoneAlarm
 from brainframe.client.api.zss_pubsub import zss_publisher
-from brainframe.client.ui.resources import stylesheet_watcher
+from brainframe.client.ui.dialogs.alarm_view.alarm_view_ui import AlarmViewUI
 from brainframe.client.ui.resources.alarms.alarm_bundle import AlarmBundle
 from brainframe.client.ui.resources.mixins.data_structure import IterableMI
-from brainframe.client.ui.resources.mixins.style import TransientScrollbarMI
-from brainframe.client.ui.resources.paths import qt_qss_paths
-
-
-class AlarmViewUI(QScrollArea, TransientScrollbarMI):
-
-    def __init__(self, parent: QWidget):
-        super().__init__(parent)
-
-        container_widget = self._init_container_widget()
-        self._init_viewport_widget()
-
-        self.setWidget(container_widget)
-
-        self._init_style()
-
-    def _init_style(self) -> None:
-        # Allow background of widget to be styled
-        self.setAttribute(Qt.WA_StyledBackground, True)
-
-        self.setWidgetResizable(True)
-
-        stylesheet_watcher.watch(self, qt_qss_paths.alarm_view_qss)
-
-    def _init_container_widget(self) -> QWidget:
-        container_widget = QWidget(self)
-        container_widget.setObjectName("container")
-        container_widget.setAttribute(Qt.WA_StyledBackground, True)
-
-        # Leave some space on the right for the scrollbar
-        contents_margins: QMargins = self.contentsMargins()
-        contents_margins.setLeft(50)
-        contents_margins.setRight(50)
-        container_widget.setContentsMargins(contents_margins)
-
-        container_widget.setLayout(self._init_container_widget_layout())
-
-        return container_widget
-
-    # noinspection PyMethodMayBeStatic
-    def _init_container_widget_layout(self) -> QVBoxLayout:
-        layout = QVBoxLayout()
-        layout.setContentsMargins(0, 0, 0, 0)
-
-        layout.setAlignment(Qt.AlignTop)
-        return layout
-
-    def _init_viewport_widget(self) -> None:
-        # Give the viewport a name for the stylesheet
-        self.viewport().setObjectName("viewport")
 
 
 class AlarmView(AlarmViewUI, IterableMI):
@@ -154,7 +104,7 @@ class AlarmView(AlarmViewUI, IterableMI):
 
 if __name__ == '__main__':
     import typing
-    from PyQt5.QtWidgets import QApplication, QDesktopWidget
+    from PyQt5.QtWidgets import QDesktopWidget
 
     api.set_url("http://localhost")
     zss = api.get_status_receiver()
