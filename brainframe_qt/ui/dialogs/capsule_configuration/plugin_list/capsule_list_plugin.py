4852082834d2b8b72e720bc8bf7515ebc98e4c04
Bryce Beagle <bryce.beagle@aotu.ai>
2020-05-20 15:12:27 -0700

    plugin_list.py -> capsule_list.py

--- brainframe_qt/ui/dialogs/capsule_configuration/plugin_list/capsule_list_plugin.py
+++ brainframe_qt/ui/dialogs/capsule_configuration/plugin_list/capsule_list_plugin.py
@@ -3,7 +3,7 @@ from brainframe.client.ui.resources.qt_plugins.base_plugin import BasePlugin
 
 class CapsuleList(BasePlugin):
     # noinspection PyUnresolvedReferences
-    from brainframe.client.ui.dialogs.capsule_configuration.plugin_list.plugin_list import \
+    from brainframe.client.ui.dialogs.capsule_configuration.plugin_list.capsule_list import \
         PluginList as Widget
 
     def __init__(self):
