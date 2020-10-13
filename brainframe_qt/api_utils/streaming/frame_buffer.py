83427f32927f3715afa30abaea3398ba15c44908
Bryce Beagle <bryce.beagle@aotu.ai>
2020-10-13 16:22:44 -0700

    Improve variable naming and comments/documentation

--- brainframe_qt/api_utils/streaming/frame_buffer.py
+++ brainframe_qt/api_utils/streaming/frame_buffer.py
@@ -8,6 +8,8 @@ if TYPE_CHECKING:
 from brainframe.client.api_utils.streaming.processed_frame import \
     ProcessedFrame
 
+_BUFFER_TYPE = List[ProcessedFrame]
+
 
 @dataclass
 class ReaderFrame:
@@ -22,14 +24,13 @@ class SyncedFrameBuffer:
     MAX_BUF_SIZE: int = 300  # Arbitrary but not too big
     _buffer_lock: ClassVar[RLock] = RLock()
 
-    combined_buffer: ClassVar[List[Tuple[ReaderFrame]]] = []
+    combined_buffer: ClassVar[List[ReaderFrame]] = []
 
-    split_buffers: ClassVar[Dict['SyncedStreamReader', List[ProcessedFrame]]]
-    split_buffers = {}
+    instance_buffers: ClassVar[Dict['SyncedStreamReader', _BUFFER_TYPE]] = {}
 
     def __init__(self, stream_reader: 'SyncedStreamReader'):
         self.stream_reader = stream_reader
-        self.split_buffers[stream_reader] = []
+        self.instance_buffers[stream_reader] = []
 
     def __del__(self):
         # This could be done by looping over self.pop, but this is faster
@@ -43,7 +44,7 @@ class SyncedFrameBuffer:
             ]
 
             # Remove dict entry to this SyncedStreamReader entirely
-            self.split_buffers.pop(self.stream_reader)
+            self.instance_buffers.pop(self.stream_reader)
 
     def add_frame(self, frame: ProcessedFrame) -> None:
         with self._buffer_lock:
@@ -56,56 +57,63 @@ class SyncedFrameBuffer:
             )
 
             bisect.insort_left(self.combined_buffer, reader_frame)
-            self.split_buffers[self.stream_reader].append(frame)
+            self._instance_buffer.append(frame)
 
     def pop_oldest(self) -> ProcessedFrame:
+        """Pop the oldest frame from this instance's buffer"""
         with self._buffer_lock:
             # Get oldest frame for stream
-            frame = self.split_buffers[self.stream_reader].pop(0)
+            frame = self._instance_buffer.pop(0)
 
             reader_frame = ReaderFrame(
                 stream_reader=self.stream_reader,
                 frame=frame
             )
 
-            # Remove it from combined deque
             # TODO: Test with really large buffer. Make sure not too
             #       computationally intensive
+            # Remove it from combined buffer
             self.combined_buffer.remove(reader_frame)
 
         return frame
 
     def pop_until(self, tstamp: float) -> Optional[ProcessedFrame]:
-        oldest_frame: Optional[ProcessedFrame]
-        buffer = self.split_buffers[self.stream_reader]
+        """Pop frames until the provided oldest frame in the buffer is newer
+        than the provided tstamp
+        """
+        oldest_frame: Optional[ProcessedFrame] = None
 
         with self._buffer_lock:
-            while len(buffer):
-                oldest_frame = buffer[0]
+            while True:
+                popped_frame = self.pop_if_older(tstamp)
 
-                # Buffer has caught up
-                if oldest_frame.tstamp >= tstamp:
+                if not popped_frame:
                     break
 
-                self.pop_oldest()
+                oldest_frame = popped_frame
 
         return oldest_frame
 
     def pop_if_older(self, tstamp: float) -> Optional[ProcessedFrame]:
-        """Pop the oldest frame in the buffer if the its tstamp is older than
-        the one supplied.
+        """Pop the oldest frame in the buffer if its tstamp is older than the
+        one supplied.
 
         :return: None if there are no frames, or if the oldest frame has a
                  newer timestamp. The ProcessedFrame, if it's older.
 
         """
         with self._buffer_lock:
-            buffer = self.split_buffers[self.stream_reader]
-            if not len(buffer):
+            if not len(self._instance_buffer):
                 return None
 
-            if buffer[0].tstamp <= tstamp:
-                return self.pop_oldest()
+            oldest_frame = self._instance_buffer[0]
+
+            if oldest_frame.tstamp < tstamp:
+                # Pop the frame from both instance and combined buffers
+                popped_frame = self.pop_oldest()
+                assert oldest_frame is popped_frame
+
+                return popped_frame
             else:
                 return None
 
@@ -116,5 +124,11 @@ class SyncedFrameBuffer:
             frame = reader_frame.frame
             stream_reader = reader_frame.stream_reader
 
-            popped_frame = self.split_buffers[stream_reader].pop(0)
-            assert frame == popped_frame
+            # Note that we're popping a frame from a potentially different
+            # instance's buffer
+            popped_frame = self.instance_buffers[stream_reader].pop(0)
+            assert frame is popped_frame
+
+    @property
+    def _instance_buffer(self) -> _BUFFER_TYPE:
+        return self.instance_buffers[self.stream_reader]
