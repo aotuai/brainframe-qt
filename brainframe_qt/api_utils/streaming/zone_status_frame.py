90191098d3396ff598bb0fbdf108cfb37e0a0e7f
Bryce Beagle <bryce.beagle@aotu.ai>
2020-11-05 17:15:11 -0800

    Change ZoneStatusFrame to a dataclass

--- brainframe_qt/api_utils/streaming/zone_status_frame.py
+++ brainframe_qt/api_utils/streaming/zone_status_frame.py
@@ -1,35 +1,35 @@
-from typing import List
+from dataclasses import dataclass, field
+from typing import Dict, List, Optional
 
 import numpy as np
 from brainframe.api.bf_codecs import ZoneStatus
+from cached_property import cached_property
 
 from brainframe.client.api_utils.detection_tracks import DetectionTrack
 
 
+@dataclass
 class ZoneStatusFrame:
     """A frame that may or may not have undergone processing on the server."""
 
-    def __init__(self, frame, tstamp, zone_statuses, has_new_statuses, tracks):
-        """
-        :param frame: RGB data on the frame
-        :param tstamp: The timestamp of the frame
-        :param zone_statuses: A zone status that is most relevant to this
-            frame, though it might not be a result of this frame specifically
-        :param has_new_statuses: True if this processed frame contains new
-            zone statuses that have not appeared in previous processed frames
-        """
-        self.frame: np.ndarray = frame
-        self.tstamp: float = tstamp
-        self.zone_statuses: List[ZoneStatus] = zone_statuses
-        self.has_new_zone_statuses = has_new_statuses
-        self.tracks: List[DetectionTrack] = tracks
+    # compare=False to prevent numpy complaining about truthiness of arrays
+    frame: np.ndarray = field(compare=False)
+    """BGR data on the frame"""
 
-        # Cachable properties
-        self._frame_rgb = None
+    tstamp: float
+    """The timestamp of the frame"""
 
-    @property
+    zone_statuses: Optional[Dict[str, ZoneStatus]]
+    """ZoneStatuses for the frame"""
+
+    has_new_statuses: bool
+    """True if this processed frame contains new zone statuses that have not
+    appeared in previous processed frames"""
+
+    tracks: Optional[List[DetectionTrack]]
+    """DetectionTrack history for the frame"""
+
+    @cached_property
     def frame_rgb(self):
         """Flip the BGR channels to RGB"""
-        if not self._frame_rgb:
-            self._frame_rgb = self.frame[..., ::-1].copy()
-        return self._frame_rgb
+        return self.frame[..., ::-1].copy()
