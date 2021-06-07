cdf52886f7f3edf69276d495e466f6920d465847
Bryce Beagle <bryce.beagle@aotu.ai>
2021-06-07 09:00:51 -0700

    Fix task config overwriting global render configuration

--- brainframe_qt/ui/resources/video_items/streams/stream_widget.py
+++ brainframe_qt/ui/resources/video_items/streams/stream_widget.py
@@ -19,6 +19,10 @@ class StreamWidget(StreamWidgetUI, StreamListenerWidget):
     def __init__(self, *, parent: QWidget):
         super().__init__(parent=parent)
 
+        self._draw_lines: Optional[bool] = None
+        self._draw_regions: Optional[bool] = None
+        self._draw_detections: Optional[bool] = None
+
     def resizeEvent(self, _event: Optional[QResizeEvent] = None) -> None:
         """Take up entire width using aspect ratio of scene"""
 
@@ -29,6 +33,39 @@ class StreamWidget(StreamWidgetUI, StreamListenerWidget):
             # The sceneRect grows but never shrinks automatically
             self.scene().setSceneRect(current_frame.boundingRect())
             self.fitInView(current_frame.boundingRect(), Qt.KeepAspectRatio)
+
+    @property
+    def draw_lines(self) -> bool:
+        if self._draw_lines is None:
+            return self.render_config.draw_lines
+        else:
+            return self._draw_lines
+
+    @draw_lines.setter
+    def draw_lines(self, draw_lines: bool):
+        self._draw_lines = draw_lines
+
+    @property
+    def draw_regions(self) -> bool:
+        if self._draw_regions is None:
+            return self.render_config.draw_regions
+        else:
+            return self._draw_regions
+
+    @draw_regions.setter
+    def draw_regions(self, draw_regions: bool):
+        self._draw_regions = draw_regions
+
+    @property
+    def draw_detections(self) -> bool:
+        if self._draw_detections is None:
+            return self.render_config.draw_detections
+        else:
+            return self._draw_detections
+
+    @draw_detections.setter
+    def draw_detections(self, draw_detections: bool):
+        self._draw_detections = draw_detections
 
     def on_frame(self, frame: ZoneStatusFrame) -> None:
 
@@ -43,13 +80,13 @@ class StreamWidget(StreamWidgetUI, StreamListenerWidget):
         if frame.zone_statuses is None:
             return
 
-        if self.render_config.draw_lines:
+        if self.draw_lines:
             self.scene().draw_lines(frame.zone_statuses)
 
-        if self.render_config.draw_regions:
+        if self.draw_regions:
             self.scene().draw_regions(frame.zone_statuses)
 
-        if self.render_config.draw_detections:
+        if self.draw_detections:
             self.scene().draw_detections(
                 frame_tstamp=frame.tstamp,
                 tracks=frame.tracks
