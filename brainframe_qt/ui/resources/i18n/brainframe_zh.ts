14d9dbf04fbb3e3cbef0aa1ac62a59ab791c15c2
Stephen Li <stephen@aotu.ai>
2022-12-20 19:00:04 -0800

    Fix the unfinished translation caused by wrong encoding

--- brainframe_qt/ui/resources/i18n/brainframe_zh.ts
+++ brainframe_qt/ui/resources/i18n/brainframe_zh.ts
@@ -1545,7 +1545,7 @@ Please recheck the entered server address.</source>
     </message>
     <message>
         <location filename="../../main_window/video_thumbnail_view/video_thumbnail_view_ui.py" line="29"/>
-        <source>Click the &#xe2;&#x9e;&#x95; button to add a new stream</source>
+        <source>Click the ➕ button to add a new stream</source>
         <translation>点击 ➕ 按钮以添加新的视频流</translation>
     </message>
 </context>
